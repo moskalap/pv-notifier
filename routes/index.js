142c363dc155f85b54a1d3420bc2d4dfac75a498
Przemysław Moskała <przemyslaw.moskala@synerise.com>
2020-10-10 00:50:39 +0200

    add gitgnore

--- routes/index.js
+++ routes/index.js
@@ -56,21 +56,24 @@ function getStationCode(headers) {
     })
 }
 
-function getDailyProduction(headers, stationCodes ) {
+async function getDailyProduction(headers, stationCodes ) {
     return fetchAndParse(`${HUAWEI}/thirdData/getStationRealKpi`, 'POST', headers, {stationCodes})
         .then(({body, response}) => {
             if(body.data[0]) {
                 const dayPower = body.data[0].dataItemMap.day_power;
 
                 const price = parseFloat(0.617 * Number(dayPower)).toFixed(2);
+
                 return {price, dayPower}
             }
             else throw new Error()
         })
 }
 
-function sendNotification(price, dayPower) {
-    var msg = {
+function sendNotification(price, dayPower, year_prod, year_use, day_prod, day_use) {
+    const day_saldo = 0.8 * Number(day_prod) - day_use
+    const year_saldo =  0.8 * Number(year_prod) - year_use
+    var msgtoday = {
         m: `Dziś wyprodukowano ${dayPower} kWh\nWartość prądu: ${price} zł.`,   // message (required)
         t: "Fotowoltaika",                     // title (optional)
         s: '8',                                // sound (value 0-50)
@@ -79,11 +82,42 @@ function sendNotification(price, dayPower) {
         c: '#FF0000',                          // iconcolor (optional)
         d: 'a'                               // the device or device group id
     };
+
+    var msgyesterday = {
+        m: `Wczoraj \noddano ${day_prod} kWh, Zużyto ${day_use}\nSaldo: ${day_saldo} Wartość prądu: ${day_saldo*0.61} zł.`,   // message (required)
+        t: "Fotowoltaika",                     // title (optional)
+        s: '8',                                // sound (value 0-50)
+        v: '2',                                // vibration (empty or value 1-3)
+        // icon (value 1-176)
+        c: '#FF0000',                          // iconcolor (optional)
+        d: 'a'                               // the device or device group id
+    };
+
+
+    var msgyear = {
+        m: `Rocznie oddano ${year_prod} kWh, Zużyto ${year_use}\nSaldo: ${year_saldo} Wartość prądu: ${year_saldo*0.61} zł.`,   // message (required)
+        t: "Fotowoltaika",                     // title (optional)
+        s: '8',                                // sound (value 0-50)
+        v: '2',                                // vibration (empty or value 1-3)
+        // icon (value 1-176)
+        c: '#FF0000',                          // iconcolor (optional)
+        d: 'a'                               // the device or device group id
+    };
+
+
     var p = new push( {
         k: 'dsI5nbrVsFfRjkb1kKUa',             // your 20 chars long private key
         debug: false
     });
-    p.send( msg, function( err, result ) {
+    p.send( msgtoday, function( err, result ) {
+        //console.log( 'ERROR:', err );
+        console.log( 'RESULT', result );
+        // process.exit(0);
+    }); p.send( msgyesterday, function( err, result ) {
+        //console.log( 'ERROR:', err );
+        console.log( 'RESULT', result );
+        // process.exit(0);
+    }); p.send( msgyear, function( err, result ) {
         //console.log( 'ERROR:', err );
         console.log( 'RESULT', result );
         // process.exit(0);
@@ -96,7 +130,12 @@ async function send_usage_stats() {
         .then(headers =>
             getStationCode(headers, )
                 .then(stationCode => getDailyProduction(headers, stationCode))
-                .then(({price, dayPower}) => sendNotification(price, dayPower))
+                .then(async ({price, dayPower}) => {
+                    const tauron = await fetch('https://pmoskala-tauron.herokuapp.com');
+                    const text = await tauron.text();
+                    [year_out, year_in, day_out, day_in] = text.replace('(', '').replace(')', '').split(',')
+                    sendNotification(price, dayPower, year_out, year_in, day_out, day_in)
+                })
 
         );
 
